e4dbbe6d9bad53d96b448b5a57c710a935bec11f
Jeremy Clark <jeremy@jeremybytes.com>
2023-10-02 14:49:34 -0700

    Added combined workflow application

--- README.md
+++ README.md
@@ -1,37 +1,44 @@
 # cassette-to-mp3
 
-Work in progress to convert audio cassettes to MP3 files. There are currently separate applications for each step of the workflow. This is a work-in-progress, and these will be combined where possible.
+Work in progress to convert audio cassettes to MP3 files. Both a combined application and separate applications for each step are available. This is a work-in-progress.
 
-*Note: These projects all use NAudio for recording, editing, and converting.*
+*Note: These projects use NAudio for recording, editing, and converting.*
 
 ## Projects
-1. WAVRecorder  
+1. WAVRecorder 
 Desktop application to record from a PC audio source (such as LineIn) to a WAV file.  
 I use this to record a side of a cassette tape from and an Akai HX-1C through a PC input.
 
-2. WAVSplitter  
+2. RecordingToMP3  
+A desktop application that splits the recording (from WAVRecorder) into separate tracks/files and converts them to MP3 with appropriate tags.  
+*Note: Double-click the "input" box to open a file dialog to pick the WAV source.*  
+The output creates folders for artist and album under the original source folder.
+
+3. AudioProcessorLibrary  
+Combined library to support entire workflow (used by RecordingToMP3).  
+
+4. WAVSplitter  
 Desktop application to split a WAV file into separate files based on provided timestamps.  
 I use this to split a recording of a full side of a cassette tape into individual tracks.  
 *Note: I get the timestamps by looking at the WAV file in an audio editor.*  
+This functionality is included in "RecordingToMP3".  
 
-3. WAVtoMP3  
+5. WAVtoMP3  
 Desktop application to convert the separate wave files into MP3s based on provided track names.  
+This functionality is included in "RecordingToMP3".  
 
-4. MP3Tagger
+6. MP3Tagger
 Desktop application to add ID3 tags to MP3 files.  
-***Work in Progress***  
+This functionality is included in "RecordingToMP3".  
 
-5. AudioProcessorLibrary  
-Combine library to support entire workflow.  
-
-6. TesterApp  
+7. TesterApp  
 Console application to test the workflow on a single, hard-coded file. (working!)
 
 ## Enhancements
 I've got the following features planned.  
 
-* Combination of splitting and converting to MP3 as a single application.  
-* Add MP3 tags (artist, album, track, year, etc.)  
+* Combination of splitting and converting to MP3 as a single application. **DONE**  
+* Add MP3 tags (artist, album, track, year, etc.) **DONE**  
 * See if I can figure out a way to "auto-split" (although I'm not sure I would trust this since albums vary in moving from track to track).  
 
 ## Notes  
---
+++ RecordingToMP3/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using AudioProcessorLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace RecordingToMP3;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Go_Click(object sender, RoutedEventArgs e)
+    {
+        // Input for raw recording
+        string inputFile = InputName.Text;
+        int baseTrack = int.Parse(TrackStart.Text);
+
+        TrackInfo baseInfo = new(
+            Artist.Text,
+            Album.Text,
+            int.Parse(Year.Text),
+            "",
+            0);
+
+        List<TimeSpan> editPoints = GetEditPoints(EditPoints.Text);
+        List<string> trackNames = GetTrackNames(TrackNames.Text);
+
+        for (int i = 1; i < trackNames.Count; i++)
+        {
+            TimeSpan start = editPoints[i - 1];
+            TimeSpan end = editPoints[i];
+            int trackNumber = baseTrack + i - 1;
+            TrackInfo trackInfo = baseInfo with { TrackNumber = trackNumber, Title = trackNames[i] };
+
+            Splitter splitter = new();
+
+            // Split out single track (to WAV)
+            string wavFilePath = splitter.ExtrackWAVTrack(
+                inputFile,
+                start,
+                end,
+                trackInfo);
+
+            // Convert WAV to MP3
+            MP3Converter converter = new();
+
+            string mp3Path = converter.ConvertFile(wavFilePath);
+
+            // Delete WAV
+            File.Delete(wavFilePath);
+
+            // Add tags to MP3
+            Tagger tagger = new();
+            tagger.TagFile(mp3Path, trackInfo);
+        }
+
+        MessageBox.Show("Done");
+    }
+
+    private void InputName_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        // Configure open file dialog box
+        var dialog = new Microsoft.Win32.OpenFileDialog();
+        dialog.InitialDirectory = """C:\Recordings""";
+        dialog.FileName = ""; // Default file name
+        dialog.DefaultExt = ".wav"; // Default file extension
+        dialog.Filter = "WAV Files (.wav)|*.wav"; // Filter files by extension
+
+        // Show open file dialog box
+        bool? result = dialog.ShowDialog();
+
+        // Process open file dialog box results
+        if (result == true)
+        {
+            // Open document
+            string filename = dialog.FileName;
+            InputName.Text = filename;
+        }
+    }
+
+    private List<TimeSpan> GetEditPoints(string text)
+    {
+        string[] times = text.Split('\n', StringSplitOptions.TrimEntries);
+        List<TimeSpan> editPoints = new();
+        foreach (string item in times)
+            editPoints.Add(TimeSpan.Parse($"00:{item}"));
+        return editPoints;
+    }
+
+    private List<string> GetTrackNames(string text)
+    {
+        List<string> tracks = new() { "EMPTY" };
+        string[] data = text.Split('\n', StringSplitOptions.TrimEntries);
+        foreach (var item in data)
+            tracks.Add(item);
+        return tracks;
+    }
+}
